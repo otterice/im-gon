a737efd029f534adbd86d9e4d61b3f5a9db7badf
Joseph Dang <dang_joseph@trustthink.net>
2023-04-29 16:29:31 -0700

    finished part3

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   const [zoom, setZoom] = useState(9);
   const [routes, setRoutes] = useState([]);
   const [waypoints, setWaypoints] = useState([]);
-
+  var directions = useRef(null);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
@@ -30,63 +30,54 @@ function App() {
     });//.addControl(directions, 'top-left');
 
     const customPoints = [
-      { name: 'Point A', coordinates: [-117.8674, 34.8818] },
-      { name: 'Point B', coordinates: [-118.8674, 35.8818] },
-      { name: 'Point C', coordinates: [-116.8674, 33.8818] }
+      { name: 'Point A', coordinates: [-117.8674,34.8818] },
+      { name: 'Point B', coordinates: [-118.8674,35.8818] },
+      { name: 'Point C', coordinates: [-118.8694,36.8818] },
+      { name: 'Point D', coordinates: [-119.8694,36.8818] },
+
     ];
 
-    var start = [-117.88476,34.89784];
-    var end = [-117.74270,34.84688];
-
-//     customPoints.forEach((point, index) => {
-//       const marker = new mapboxgl.Marker()
-//         .setLngLat(point.coordinates)
-//         .addTo(map.current);
-// });
-
     var apiReq = `https://api.mapbox.com/directions/v5/mapbox/driving/`;
-
-    /*
-
-    -117.8674%2C34.8818%3B
-    -118.8674%2C35.8818%3B
-    -116.8674%2C33.8818
-
-    */
-
 
     customPoints.forEach((point, index) => {
       apiReq += point.coordinates[0] + '%2C' + point.coordinates[1] + '%3B';
     });
     apiReq=apiReq.slice(0,-3);
-
     apiReq += `?alternatives=true&geometries=geojson&language=en&overview=simplified&steps=true&access_token=${mapboxgl.accessToken}`;
 
-    
     const getData = async () => {
       await axios.get(
       apiReq)
-      //`https://api.mapbox.com/directions/v5/mapbox/driving/${customPoints[0].coordinates[0]},${customPoints[0].coordinates[1]};${customPoints[1].coordinates[0]},${customPoints[1].coordinates[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`)
       .then(response => {
         routes.push((response.data.routes)); 
         waypoints.push((response.data.waypoints));
-        console.log(response.data);
-        console.log(routes);
-        console.log(waypoints);
-      });
-    }
 
-    var directions = new MapboxDirections({
-      accessToken: mapboxgl.accessToken,
-      unit: 'metric',
-      profile: 'mapbox/driving',
-      routes: routes,
-      waypoints: waypoints 
-    });
+        const instructions = document.getElementById('instructions');
+        let time = 0;
+        let tripInstructions = '';
+       
+        routes[0].forEach((route) => {
+          time += route.duration;
+          route.legs.forEach((leg) => {
+            leg.steps.forEach((step) => {
+              tripInstructions += `<li>${step.maneuver.instruction}</li>`;
 
-    getData();
+              console.log(step.maneuver.instruction);
+            })
+          })
+        });
 
-    map.current.addControl(directions, 'top-left');
+        instructions.innerHTML = `<p><strong>Trip duration: ${Math.floor(
+          time / 60
+        )} min 🚴 </strong></p><ol>${tripInstructions}</ol>`;
+
+        directions = new MapboxDirections({
+          accessToken: mapboxgl.accessToken,
+          unit: 'metric',
+          profile: 'mapbox/driving',
+          controls:{instructions:false}
+          
+        });
 
     map.current.on('move', () => {
       setLng(map.current.getCenter().lng.toFixed(4));
@@ -94,41 +85,34 @@ function App() {
       setZoom(map.current.getZoom().toFixed(2));
     });
 
-    console.log(start,end);
-
-    console.log('waypoints after' + waypoints);
-
     map.current.on('load', function() {
-      directions.setOrigin(waypoints[0]);
-      directions.addWaypoint(0,waypoints[1]);
-      directions.setDestination(waypoints[2]);
-      // directions.setOrigin(start); 
-      // directions.addWaypoint(0, [-117.86414,34.73640]);
-      // const marker = new mapboxgl.Marker()
-      // .setLngLat([-117.86414,34.73640])
-      // .addTo(map.current);
-      // directions.addWaypoint(1, [-117.97031,34.72134]);
-      // directions.setDestination(end);  
+      directions.setOrigin(waypoints[0][0].location);
+      for (let i = 1; i < waypoints[0].length-1; i++) {
+      directions.addWaypoint(i, waypoints[0][i].location);
+        const marker = new mapboxgl.Marker()
+        .setLngLat(waypoints[0][i].location)
+        .addTo(map.current);   
+      }
+      directions.setDestination(waypoints[0][waypoints[0].length-1].location);      
     });
-  
-  
+      map.current.addControl(directions, 'top-left');
+      });
+    }
+
+    getData();
   });
     
   return (
     <div className="App">
       <header className="App-header">
-        <div className="sidebar">
-          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-        </div>
-        <div ref={mapContainer} style={{ width: "100%", height: "600px" }}></div>
+        <div ref={mapContainer} style={{ width: "100%", height: "600px" }}> </div>
+        <div id="instructions"></div>
       </header>
       <link href='https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.css' rel='stylesheet' />
       <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.1/mapbox-gl-directions.css" type="text/css"></link>
 
     </div>
-  );
-
-  
+  );  
 }
 
 export default App;
