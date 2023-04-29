9009d1dbb8f0fa259b80229f1fdebe32423994ee
Daniel Do <dakdo2001@gmail.com>
2023-04-29 16:17:49 -0700

    updated app.py and yelp.py
    app.py removed comments and changed function name.
    yelp.py added categories

--- app/app.py
+++ app/app.py
@@ -1,46 +1,13 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route('/', methods=['GET', 'POST'])
-# def welcome():
-#     return "Hello World!"
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=105)
-
-# from flask import Flask
-# import requests
-# import json
-# import yelp
-
-# app = Flask(__name__)
-
-# @app.route('/hello/', methods=['GET', 'POST'])
-# def welcome():
-#     return "Hello World!"
-
-# @app.route('/coffee-shops/', methods=['GET'])
-# def coffee_shops():
-#     lat = 37.7749
-#     long = -122.4194
-#     api_response = yelp.around(lat, long,169)
-#     print(api_response)
-#     return api_response
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=105)
-
 from flask import Flask, jsonify
 import yelp
 
 app = Flask(__name__)
 
-@app.route('/coffee-shops/', methods=['GET'])
-def coffee_shops():
-    lat = 37.7749
-    long = -122.4194
-    results = yelp.around(lat, long, 169)
+@app.route('/', methods=['GET'])
+def nearby_fun(): 
+    lat = 33.975600
+    long = -117.357151
+    results = yelp.around(lat, long, 1690)
     return jsonify(results)
 
 if __name__ == '__main__':
--- app/yelp.py
+++ app/yelp.py
@@ -1,17 +1,19 @@
 import requests
 import json
 
-api_key = 'lqV8h9Ut1h-q5K49ZDz0Dv9t2ZwFZVZ6HI-DjWTr9TXqMFlzO5NbDbxMoSqJPJVNIaMYND6qTYtWqeUtraucXy0sQ943Yvi3fAfqAgKMF9DYB-eYKnkxMyRbiFFNZHYx'
+api_key = 'rc4ej70SD-kM1FD3wq4BHYRz7B3Ama_fm7qfwC3lU93sjvUpwxLHUhCaoacW9H--7yfCn-ehX-q_RlyUcUFrMS8NHiY-QzwErUrkqlQvWb4Fe_Viacc2f3ugF5dNZHYx'
 
 #where you are/start/currently reside
-lat = 33.975756978566686
-long = -117.3389807714277
-rad = 1609
+# lat = 33.975756978566686
+# long = -117.3389807714277
+# rad = 1609
+
+categories = "Active Life, Arts & Entertainment,Food, Hotels & Travel,Nightlife, Restaurants, Shopping"
 
 def around(lat,long,rad):
     headers = {'Authorization': 'Bearer {}'.format(api_key)}
-    search_api_url = "https://api.yelp.com/v3/businesses/search?latitude={}&longitude={}&radius={}".format(lat, long, rad)
-    params = {'term': 'coffee', 'location': 'Toronto, Ontario','limit': 50}
+    search_api_url = "https://api.yelp.com/v3/businesses/search?latitude={}&longitude={}&radius={}&categories={}".format(lat, long, rad, categories)
+    params = {'term': 'coffee','limit': 50}
 
     response = requests.get(search_api_url, headers=headers, params=params, timeout=5)
 
@@ -30,64 +32,4 @@ def around(lat,long,rad):
         json.dump(results, f, indent=4)
 
     return results
-
-# import requests
-# import json
-
-# api_key = 'lqV8h9Ut1h-q5K49ZDz0Dv9t2ZwFZVZ6HI-DjWTr9TXqMFlzO5NbDbxMoSqJPJVNIaMYND6qTYtWqeUtraucXy0sQ943Yvi3fAfqAgKMF9DYB-eYKnkxMyRbiFFNZHYx'
-
-# lat = 37.7749
-# long = -122.4194 
-# rad = 500
-
-# headers = {'Authorization': 'Bearer {}'.format(api_key)}
-# search_api_url = "https://api.yelp.com/v3/businesses/search?latitude={}&longitude={}&radius={}".format(lat, long, rad)
-# params = {'term': 'coffee', 'location': 'California, Riverside','limit': 50}
-
-# response = requests.get(search_api_url, headers=headers, params=params, timeout=5)
-
-# if response.status_code == 200:
-#     data = response.json()
-#     if 'businesses' in data:
-#         businesses = data['businesses']
-#         if len(businesses) > 0:
-#             first_business = businesses[0]
-#             location = {
-#                 'name': first_business['name'],
-#                 'city': first_business['location']['city'],
-#                 'address': first_business['location']['address1'],
-#                 'coordinates': {
-#                     'latitude': first_business['coordinates']['latitude'],
-#                     'longitude': first_business['coordinates']['longitude']
-#                 }
-#             }
-#             with open('location.json', 'w') as f:
-#                 json.dump(location, f, indent=4)
-#     else:
-#         print("No businesses found.")
-# else:
-#     print("Request failed with status code: {}".format(response.status_code))
-
-# import requests
-# import json
-
-# api_key= 'lqV8h9Ut1h-q5K49ZDz0Dv9t2ZwFZVZ6HI-DjWTr9TXqMFlzO5NbDbxMoSqJPJVNIaMYND6qTYtWqeUtraucXy0sQ943Yvi3fAfqAgKMF9DYB-eYKnkxMyRbiFFNZHYx'
-
-# #where you are/start/currently reside
-# lat = 33.975756978566686
-# long = -117.3389807714277
-
-# headers = {'Authorization': 'Bearer {}'.format(api_key)}
-# search_api_url = "https://api.yelp.com/v3/businesses/search?latitude={}&longitude={}&radius=1609".format(lat, long)
-# params = {'term': 'coffee', 'location': 'Toronto, Ontario','limit': 50}
-
-# response = requests.get(search_api_url, headers=headers, params=params, timeout=5)
-
-# data = json.loads(response.text)
-
-# for business in data['businesses']:
-#     print("Latitude: {}, Longitude: {}".format(business['coordinates']['latitude'], business['coordinates']['longitude']),
-#           business['name'], 
-#           business['location']['address1'], 
-#           business['location']['city'])
 
