4a1d60e38567299da692a04ab1e84964e421a0d4
Daniel Do <dakdo2001@gmail.com>
2023-04-29 19:14:08 -0700

    f
    f

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import {
 import AllRoutes from "./AllRoutes"
 import MarkRoutes from "./MarkRoutes"
 
+import Home from "./Home";
 
 const App = () => {
   return (
+<<<<<<< Updated upstream
     <div className="App">
       <header className="App-header">
         <div className="sidebar">
@@ -21,6 +23,15 @@ const App = () => {
       <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.1/mapbox-gl-directions.css" type="text/css"></link>
 
     </div>
+=======
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/allRoutes" element={<AllRoutes />} />
+        <Route path="/markRoutes" element={<MarkRoutes/>}/>
+      </Routes>
+    </Router>
+>>>>>>> Stashed changes
   );
 }
 
