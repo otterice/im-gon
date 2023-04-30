0b054c658e10f6e4cc99289dd4d29ecd74a03ddf
Daniel Do <dakdo2001@gmail.com>
2023-04-30 00:10:41 -0700

    made some css adjustments
    now it looks aweseom

--- src/screen1.js
+++ src/screen1.js
@@ -20,32 +20,42 @@ function Screen1() {
   }
 
   return (
-    <div>
-      <h1>Enter Starting and Ending Coordinates</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Starting Latitude:
-          <input type="text" value={startLat} onChange={(e) => setStartLat(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Starting Longitude:
-          <input type="text" value={startLong} onChange={(e) => setStartLong(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Ending Latitude:
-          <input type="text" value={endLat} onChange={(e) => setEndLat(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Ending Longitude:
-          <input type="text" value={endLong} onChange={(e) => setEndLong(e.target.value)} />
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-      </form> 
+<div style={{ background: 'linear-gradient(45deg, #25da5e 10%, #5939c6 200%)', minHeight: '100vh', margin: 0, padding: 0, overflow: 'hidden'}}>
+  <h1 style={{ textAlign: 'center' }}>Enter Starting and Ending Coordinates</h1>
+    <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+      <label style={{ width: '150px', display: 'flex', justifyContent: 'flex-end' }}>
+        Starting Latitude:
+      </label>
+      <input type="text" value={startLat} onChange={(e) => setStartLat(e.target.value)} style={{ width: '150px', height: '30px', fontSize: '16px' }} />
     </div>
+    <br />
+    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+      <label style={{ width: '150px', display: 'flex', justifyContent: 'flex-end' }}>
+        Starting Longitude:
+      </label>
+      <input type="text" value={startLong} onChange={(e) => setStartLong(e.target.value)} style={{ width: '150px', height: '30px', fontSize: '16px' }} />
+    </div>
+    <br />
+    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+      <label style={{ width: '150px', display: 'flex', justifyContent: 'flex-end' }}>
+        Ending Latitude:
+      </label>
+      <input type="text" value={endLat} onChange={(e) => setEndLat(e.target.value)} style={{ width: '150px', height: '30px', fontSize: '16px' }} />
+    </div>
+    <br />
+    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+      <label style={{ width: '150px', display: 'flex', justifyContent: 'flex-end' }}>
+        Ending Longitude:
+      </label>
+      <input type="text" value={endLong} onChange={(e) => setEndLong(e.target.value)} style={{ width: '150px', height: '30px', fontSize: '16px', textIndent: '5px'}} />
+    </div>
+    <br />
+    <button type="submit" style={{ width: '100px', height: '30px', fontSize: '20px' }}>Submit</button>
+  </form> 
+</div>
+
+
   );
 }
 
