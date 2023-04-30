7f70d22c7bd547200170d159bc8efd39a9b16e11
Daniel Do <dakdo2001@gmail.com>
2023-04-29 18:44:01 -0700

    stuff

---
+++ src/screen1.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [startLat, setStartLat] = useState('');
+  const [startLong, setStartLong] = useState('');
+  const [endLat, setEndLat] = useState('');
+  const [endLong, setEndLong] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Do something with the input values, e.g. update the state
+  }
+
+  return (
+    <div>
+      <h1>Enter Starting and Ending Coordinates</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Starting Latitude:
+          <input type="text" value={startLat} onChange={(e) => setStartLat(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Starting Longitude:
+          <input type="text" value={startLong} onChange={(e) => setStartLong(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Ending Latitude:
+          <input type="text" value={endLat} onChange={(e) => setEndLat(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Ending Longitude:
+          <input type="text" value={endLong} onChange={(e) => setEndLong(e.target.value)} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
